c05e664de71dab7397434eadda9d5e33c111a3d0
Sumaira-Shaikh <sumaira92shaikh@gmail.com>
2025-12-06 23:11:01 +0500

    Describe your changes

---
+++ docusaurus.config.js
@@ -0,0 +1,26 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Humanoid Robotics Book',
+  tagline: 'AI + Robotics',
+  url: 'https://your-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'your-org',
+  projectName: 'humanoid-robotics-book',
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
